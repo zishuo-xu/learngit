0a53667f729699887811204976f80ab82aeebb98
shamir_xu <shamir_xu@digifinex.org>
2022-06-01 16:53:03 +0800

    redis_study

--- go.mod
+++ go.mod
@@ -2,9 +2,19 @@ module golang_study
 
 go 1.17
 
-require github.com/jinzhu/gorm v1.9.16
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.uber.org/zap v1.21.0
+)
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/BurntSushi/toml v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/onsi/gomega v1.19.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,44 +2,128 @@ package main
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/go-redis/redis"
+	ormlog "golang_study/log"
+	ormredis "golang_study/redis"
 )
 
-// UserInfo 用户信息
-type UserInfo struct {
-	ID     uint
-	Name   string
-	Gender string
-	Hobby  string
+func init() {
+	ormlog.InitLogger()
+	err := ormredis.InitClient()
+	if err != nil {
+		ormlog.SugarLogger.Error("连接错误")
+	} else {
+		ormlog.SugarLogger.Infof("连接成功 host:%s,port:%s", "127.0.0.1", "6379")
+	}
 }
 
 func main() {
-	db, err := gorm.Open("mysql", "root:iwala.net007@(10.200.200.111:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local")
-	if err != nil {
+	/*
+		orm.InitLogger()
+		defer orm.SugarLogger.Sync()
+		//error
+		orm.SugarLogger.Error("这是error")
+		orm.SugarLogger.Errorf("这是%s", "error")
+		//info
+		orm.SugarLogger.Info("这是info")
+		orm.SugarLogger.Infof("这是%s", "info")
+		orm.SugarLogger.Infow("这是info，key and values", "key", "value")
+		//debug
+		orm.SugarLogger.Debug("这是debug")
+		orm.SugarLogger.Debugf("这是%s", "debug")
+		orm.SugarLogger.Debugw("这是debug，key and values", "key", "value")
+		//warm
+		orm.SugarLogger.Warn("这是warm")
+		orm.SugarLogger.Warnf("这是%s", "warm")
+		orm.SugarLogger.Warnw("这是warm，key and values", "key", "value")
+
+	*/
+	//redisExample2()
+	iter := ormredis.Rdb.Scan(0, "s*", 0).Iterator()
+	for iter.Next() {
+		err := ormredis.Rdb.Del(iter.Val()).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+	if err := iter.Err(); err != nil {
 		panic(err)
 	}
-	defer db.Close()
+}
 
-	// 自动迁移
-	db.AutoMigrate(&UserInfo{})
+//set/get示例
+func redisExample() {
+	err := ormredis.Rdb.Set("score", 100, 0).Err()
+	if err != nil {
+		fmt.Printf("set score failed, err:%v\n", err)
+		return
+	}
 
-	u1 := UserInfo{1, "七米", "男", "篮球"}
-	u2 := UserInfo{2, "沙河娜扎", "女", "足球"}
-	// 创建记录
-	db.Create(&u1)
-	db.Create(&u2)
-	// 查询
-	var u = new(UserInfo)
-	db.First(u)
-	fmt.Printf("%#v\n", u)
+	val, err := ormredis.Rdb.Get("score").Result()
+	if err != nil {
+		fmt.Printf("get score failed, err:%v\n", err)
+		return
+	}
+	fmt.Println("score", val)
 
-	var uu UserInfo
-	db.Find(&uu, "hobby=?", "足球")
-	fmt.Printf("%#v\n", uu)
+	val2, err := ormredis.Rdb.Get("name").Result()
+	if err == redis.Nil {
+		fmt.Println("name does not exist")
+	} else if err != nil {
+		fmt.Printf("get name failed, err:%v\n", err)
+		return
+	} else {
+		fmt.Println("name", val2)
+	}
+}
 
-	// 更新
-	db.Model(&u).Update("hobby", "双色球")
-	// 删除
-	db.Delete(&u)
+//zset示例
+func redisExample2() {
+	zsetKey := "language_rank"
+	languages := []redis.Z{
+		redis.Z{Score: 90.0, Member: "Golang"},
+		redis.Z{Score: 98.0, Member: "Java"},
+		redis.Z{Score: 95.0, Member: "Python"},
+		redis.Z{Score: 97.0, Member: "JavaScript"},
+		redis.Z{Score: 99.0, Member: "C/C++"},
+	}
+	// ZADD
+	num, err := ormredis.Rdb.ZAdd(zsetKey, languages...).Result()
+	if err != nil {
+		fmt.Printf("zadd failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("zadd %d succ.\n", num)
+
+	// 把Golang的分数加10
+	newScore, err := ormredis.Rdb.ZIncrBy(zsetKey, 10.0, "Golang").Result()
+	if err != nil {
+		fmt.Printf("zincrby failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("Golang's score is %f now.\n", newScore)
+
+	// 取分数最高的3个
+	ret, err := ormredis.Rdb.ZRevRangeWithScores(zsetKey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("zrevrange failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
+
+	// 取95~100分的
+	op := redis.ZRangeBy{
+		Min: "95",
+		Max: "100",
+	}
+	ret, err = ormredis.Rdb.ZRangeByScoreWithScores(zsetKey, op).Result()
+	if err != nil {
+		fmt.Printf("zrangebyscore failed, err:%v\n", err)
+		return
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
 }
